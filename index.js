a2a5ff4b4ed9898683a88f7d97c4b73d76124ab0
Will Metzler <metzlerwill@gmail.com>
2024-03-18 10:55:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = (customerName.toUpperCase());
+};
+
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+};
+
+const leastFavoriteCustomer = 'Steve'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'not Steve';
+};
+
